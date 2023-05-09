48c7362023ccf8681cc671eb30d4090de20d99ca
Shachar Hananya <shacharhananya@gmail.com>
2023-05-09 03:28:21 +0300

    Change repo structure

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from distutils.core import setup
+from setuptools import find_packages
+
 setup(
-  name = 'SpaGoG',         # How you named your package folder (MyLib)
-  packages = ['SpaGoG'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Sparse data classification using Graph of Graphs models',   # Give a short description about your library
-  author = 'Shachar Hananya',                   # Type in your name
-  author_email = 'shacharhananya@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/HananyaS/SpaGoG',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/HananyaS/SpaGoG/archive/refs/tags/v_0.1.tar.gz',    # I explain this later on
-  keywords = ["GoG"],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'numpy',
-          'pandas',
-          'torch',
-          'torch_geometric',
-      ],
+    name='SpaGoG',
+    packages=find_packages(),
+    version='0.1',
+    license='MIT',
+    description='Sparse data classification using Graph of Graphs models',
+    author='Shachar Hananya',
+    author_email='shacharhananya@gmail.com',
+    url='https://github.com/HananyaS/SpaGoG',
+    download_url='https://github.com/HananyaS/SpaGoG/archive/refs/tags/v_0.1.tar.gz',
+    keywords=["GoG"],
+    install_requires=[
+        'pandas',
+        'torch',
+        'torch_geometric',
+    ],
 )
