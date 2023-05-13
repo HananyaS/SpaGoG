5c719d32d8ac53d01e76de60484afead41046e5b
Shachar Hananya <shacharhananya@gmail.com>
2023-05-13 03:06:25 +0300

    Test commit

--- spagog/main.py
+++ spagog/main.py
@@ -7,7 +7,7 @@ import torch
 import pandas as pd
 import numpy as np
 
-from .experiments import run_gc, run_gnc, run_gc_nc, get_default_params_file, get_tab_data
+from .experiments import run_gc, run_gnc, run_gc_nc, get_tab_data
 from .default_params.load_params import load_params
 
 warnings.filterwarnings("ignore")
@@ -152,6 +152,7 @@ def gog_model(
 
 
 if __name__ == "__main__":
+    print("Works")
     train_all = pd.read_csv("data/Tabular/Ecoli/processed/train.csv")
     val_all = pd.read_csv("data/Tabular/Ecoli/processed/val.csv")
     test_all = pd.read_csv("data/Tabular/Ecoli/processed/test.csv")
