41f844a59d5a17ee5c98974e1363b4c55c0352b6
HananyaS <shacharhananya@gmail.com>
2023-06-24 01:11:44 +0300

    changed setup.py to 0.15 version

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='SpaGoG',  # How you named your package folder (MyLib)
     packages=['spagog'],  # Chose the same as "name"
-    version='0.14',  # Start with a small number and increase it with every change you make
+    version='0.15',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Sparse data classification using Graph of Graphs',  # Give a short description about your library
     author='Shachar Hananya',  # Type in your name
     author_email='shacharhananya@gmail.com',  # Type in your E-Mail
     url='https://github.com/HananyaS/SpaGoG',  # Provide either the link to your github or to your website
-    download_url='https://github.com/HananyaS/SpaGoG/archive/refs/tags/v_0.14.tar.gz',  # I explain this later on
+    download_url='https://github.com/HananyaS/SpaGoG/archive/refs/tags/v_0.15.tar.gz',  # I explain this later on
     keywords=['GoG', 'Missing values', 'Graphs'],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
         'pandas==2.0.1',
