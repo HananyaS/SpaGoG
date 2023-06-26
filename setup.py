2960122540dd0ddd167c4d444d698a516e8cc85a
HananyaS <shacharhananya@gmail.com>
2023-06-26 10:54:43 +0300

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
+import setuptools
 from distutils.core import setup
 
 setup(
     name='SpaGoG',  # How you named your package folder (MyLib)
-    packages=['spagog'],  # Chose the same as "name"
-    version='0.19',  # Start with a small number and increase it with every change you make
+    packages=setuptools.find_packages(),  # Chose the same as "name"
+    version='0.2',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Sparse data classification using Graph of Graphs',  # Give a short description about your library
     author='Shachar Hananya',  # Type in your name
     author_email='shacharhananya@gmail.com',  # Type in your E-Mail
     url='https://github.com/HananyaS/SpaGoG',  # Provide either the link to your github or to your website
-    download_url='https://github.com/HananyaS/SpaGoG/archive/refs/tags/v_0.1.9.tar.gz',  # I explain this later on
+    download_url='https://github.com/HananyaS/SpaGoG/archive/refs/tags/v_0.2.tar.gz',  # I explain this later on
     keywords=['GoG', 'Missing values', 'Graphs'],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
         'pandas',
