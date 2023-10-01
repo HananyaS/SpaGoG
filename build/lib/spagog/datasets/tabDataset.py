fixed g_i construction, added learned outputs weighting and updated the params list

build/lib/spagog/datasets/tabDataset.py
@@ -17,13 +17,13 @@ class TabDataset:
     _input_types = Union[torch.Tensor, np.ndarray]
 
     def __init__(
-            self,
-            name: str,
-            train: TabDataPair,
-            test: TabDataPair = None,
-            val: TabDataPair = None,
-            normalize: bool = False,
-            feature_selection: int = None,
+        self,
+        name: str,
+        train: TabDataPair,
+        test: TabDataPair = None,
+        val: TabDataPair = None,
+        normalize: bool = False,
+        feature_selection: int = None,
     ):
         self.name = name
         self.normalized = False
@@ -34,21 +34,25 @@ class TabDataset:
 
         if self.test_exists:
             assert (
-                    test.get_num_features() == train.get_num_features()
+                test.get_num_features() == train.get_num_features()
             ), "Test doesn't have the same number of features as in train"
             self.test = test
             self.test.denormalize(inplace=True)
 
         if self.val_exists:
             assert (
-                    val.get_num_features() == train.get_num_features()
+                val.get_num_features() == train.get_num_features()
             ), "Validation doesn't have the same number of features as in train"
             self.val = val
             self.val.denormalize(inplace=True)
 
         unique_vals = self.remove_feats()
 
-        is_one_hot = lambda i: len(unique_vals[i]) == 2 and 0 in unique_vals[i] and 1 in unique_vals[i]
+        is_one_hot = (
+            lambda i: len(unique_vals[i]) == 2
+            and 0 in unique_vals[i]
+            and 1 in unique_vals[i]
+        )
         one_hot_feats = list(filter(is_one_hot, range(len(unique_vals))))
 
         self.one_hot_feats = one_hot_feats
@@ -57,8 +61,8 @@ class TabDataset:
             self.zscore()
 
         if (
-                feature_selection is not None
-                and 1 < int(feature_selection) < self.train.get_num_features()
+            feature_selection is not None
+            and 1 < int(feature_selection) < self.train.get_num_features()
         ):
             print(f"Selecting best {int(feature_selection)} features!")
             X_train_np = self.train.X.numpy()
@@ -71,8 +75,8 @@ class TabDataset:
 
             mi_scores = mutual_info_classif(X_train_np, Y_train_np)
             idx2remain = np.argpartition(mi_scores, kth=-int(feature_selection))[
-                         -int(feature_selection):
-                         ]
+                -int(feature_selection) :
+            ]
 
             self.train.X = self.train.X[:, idx2remain]
             self.val.X = self.val.X[:, idx2remain]
@@ -88,18 +92,18 @@ class TabDataset:
 
     @classmethod
     def from_attributes(
-            cls,
-            name: str,
-            train_X: _input_types,
-            train_Y: _input_types = None,
-            test_X: _input_types = None,
-            test_Y: _input_types = None,
-            val_X: _input_types = None,
-            val_Y: _input_types = None,
-            shuffle: bool = False,
-            add_existence_cols: bool = False,
-            normalize: bool = True,
-            **kwargs,
+        cls,
+        name: str,
+        train_X: _input_types,
+        train_Y: _input_types = None,
+        test_X: _input_types = None,
+        test_Y: _input_types = None,
+        val_X: _input_types = None,
+        val_Y: _input_types = None,
+        shuffle: bool = False,
+        add_existence_cols: bool = False,
+        normalize: bool = True,
+        **kwargs,
     ):
         assert test_X is not None or test_Y is None
         assert val_X is not None or val_Y is None
@@ -183,21 +187,36 @@ class TabDataset:
 
         unique_vals = list(map(find_unique_vals, self.train.X.T))
 
-        feats2remain = list(filter(lambda i: len(unique_vals[i]) > 1, range(len(unique_vals))))
+        feats2remain = list(
+            filter(lambda i: len(unique_vals[i]) > 1, range(len(unique_vals)))
+        )
 
         self.train.X = self.train.X[:, feats2remain]
-        self.test.X = self.test.X[:, feats2remain]
-        self.val.X = self.val.X[:, feats2remain]
+
+        if self.test_exists:
+            self.test.X = self.test.X[:, feats2remain]
+        if self.val_exists:
+            self.val.X = self.val.X[:, feats2remain]
+
         unique_vals = [unique_vals[i] for i in feats2remain]
 
         if self.train.normalized:
-            self.train.norm_params = self.train.norm_params[0][feats2remain], self.train.norm_params[1][feats2remain]
+            self.train.norm_params = (
+                self.train.norm_params[0][feats2remain],
+                self.train.norm_params[1][feats2remain],
+            )
 
-        if self.test.normalized:
-            self.test.norm_params = self.test.norm_params[0][feats2remain], self.test.norm_params[1][feats2remain]
+        if self.test_exists and self.test.normalized:
+            self.test.norm_params = (
+                self.test.norm_params[0][feats2remain],
+                self.test.norm_params[1][feats2remain],
+            )
 
-        if self.val.normalized:
-            self.val.norm_params = self.val.norm_params[0][feats2remain], self.val.norm_params[1][feats2remain]
+        if self.val_exists and self.val.normalized:
+            self.val.norm_params = (
+                self.val.norm_params[0][feats2remain],
+                self.val.norm_params[1][feats2remain],
+            )
 
         return unique_vals
 
@@ -219,7 +238,9 @@ class TabDataset:
         if self.train.normalized:
             self.train.denormalize(inplace=True)
 
-        _, mu, sigma = self.train.zscore(inplace=True, return_params=True, one_hot_features=self.one_hot_feats)
+        _, mu, sigma = self.train.zscore(
+            inplace=True, return_params=True, one_hot_features=self.one_hot_feats
+        )
 
         mu[self.one_hot_feats] = 0
         sigma[self.one_hot_feats] = 1
@@ -308,33 +329,42 @@ class TabDataset:
 
     @classmethod
     def load(
-            cls,
-            train_X: pd.DataFrame = None,
-            train_Y: pd.DataFrame = None,
-            val_X: pd.DataFrame = None,
-            val_Y: pd.DataFrame = None,
-            test_X: pd.DataFrame = None,
-            test_Y: pd.DataFrame = None,
-            name: str = "",
-            **kwargs,
+        cls,
+        train_X: pd.DataFrame = None,
+        train_Y: pd.DataFrame = None,
+        val_X: pd.DataFrame = None,
+        val_Y: pd.DataFrame = None,
+        test_X: pd.DataFrame = None,
+        test_Y: pd.DataFrame = None,
+        split_if_no_val: bool = False,
+        name: str = "",
+        **kwargs,
     ):
         if train_X is not None and train_Y is not None and test_X is not None:
-            if val_X is None or val_Y is None:
-                train_X, val_X, train_Y, val_Y = train_test_split(train_X, train_Y, test_size=0.2, random_state=42)
+            if (val_X is None or val_Y is None) and split_if_no_val:
+                train_X, val_X, train_Y, val_Y = train_test_split(
+                    train_X, train_Y, test_size=0.2, random_state=42
+                )
 
             train_X = train_X.reset_index(drop=True)
             train_Y = train_Y.reset_index(drop=True)
-            val_X = val_X.reset_index(drop=True)
-            val_Y = val_Y.reset_index(drop=True)
-
             train_X = train_X.values
             train_Y = train_Y.values
-            val_X = val_X.values
-            val_Y = val_Y.values
             test_X = test_X.values
 
             if test_Y is not None:
                 test_Y = test_Y.values
+
+            if val_X is not None and val_Y is not None:
+                val_X = val_X.reset_index(drop=True)
+                val_Y = val_Y.reset_index(drop=True)
+
+                val_X = val_X.values
+                val_Y = val_Y.values
+
+            else:
+                val_X = None
+                val_Y = None
 
             return cls.from_attributes(
                 train_X=train_X,
@@ -355,21 +385,32 @@ class TabDataset:
         next_idx = all_data_X.shape[0]
 
         if self.val_exists:
-            val_data_X = self.get_val_data(as_loader=False).X
-            all_data_X = torch.cat((all_data_X, val_data_X))
-            all_data_Y = torch.cat((all_data_Y, self.get_val_data(as_loader=False).Y))
-            val_mask = list(range(next_idx, next_idx + val_data_X.shape[0]))
+            val_data = self.get_val_data(as_loader=False)
+            all_data_X = torch.cat((all_data_X, val_data.X))
+            
+            if val_data.Y is not None:
+                val_data_Y = val_data.Y
+            else:
+                val_data_Y = torch.zeros(val_data.X.shape[0]) * np.nan
+            
+            all_data_Y = torch.cat((all_data_Y, val_data_Y))
+            val_mask = list(range(next_idx, next_idx + val_data.X.shape[0]))
             next_idx = all_data_X.shape[0]
 
         else:
             val_mask = None
 
         if self.test_exists:
-            test_data_X = self.get_test_data(as_loader=False).X
-            all_data_X = torch.cat((all_data_X, test_data_X))
-            if self.test.Y is not None:  # added if
-                all_data_Y = torch.cat((all_data_Y, self.get_test_data(as_loader=False).Y))
-            test_mask = list(range(next_idx, next_idx + test_data_X.shape[0]))
+            test_data = self.get_test_data(as_loader=False)
+            all_data_X = torch.cat((all_data_X, test_data.X))
+            
+            if test_data.Y is not None:
+                test_data_Y =test_data.Y
+            else:
+                test_data_Y = torch.zeros(test_data.X.shape[0]) * np.nan
+                
+            all_data_Y = torch.cat((all_data_Y, test_data_Y))
+            test_mask = list(range(next_idx, next_idx + test_data.X.shape[0]))
 
         else:
             test_mask = None

build/lib/spagog/experiments.py
@@ -22,6 +22,8 @@ def run_gc(
     evaluate_metrics: bool = True,
     probs: bool = False,
     to_numpy: bool = True,
+    f2m: bool = False,
+    n_gcn_layers: int = 1,
 ):
     graphs_dataset, *_ = GraphsDataset.from_tab(
         tab_data=tab_dataset,
@@ -31,11 +33,15 @@ def run_gc(
             "k": params.get("k", 3),
         },
         calc_intra_edges=True,
+        f2m=f2m
     )
 
-    train_loader = graphs_dataset.train.to_loader(batch_size=params["batch_size"])
-    val_loader = graphs_dataset.val.to_loader(batch_size=params["batch_size"])
-    test_loader = graphs_dataset.test.to_loader(batch_size=params["batch_size"])
+    train_loader = graphs_dataset.get_train_data(as_loader=True, batch_size=params["batch_size"])
+    val_loader = graphs_dataset.get_val_data(as_loader=True, batch_size=params["batch_size"])
+    test_loader = graphs_dataset.get_test_data(as_loader=True, batch_size=params["batch_size"])
+
+    # val_loader = graphs_dataset.val.to_loader(batch_size=params["batch_size"])
+    # test_loader = graphs_dataset.test.to_loader(batch_size=params["batch_size"])
 
     RECEIVED_PARAMS = {
         "preweight": params["preweight"],
@@ -50,6 +56,7 @@ def run_gc(
         RECEIVED_PARAMS=RECEIVED_PARAMS,
         init_weights=params.get("init", "xavier_normal"),
         embedding_layer="first",
+        n_gcn_layers=n_gcn_layers,
     )
 
     early_stopping = params.get("early_stopping", 30)
@@ -60,7 +67,7 @@ def run_gc(
         test_loader=test_loader,
         lr=params.get("lr", 0.01),
         early_stopping=early_stopping,
-        n_epochs=600,
+        n_epochs=params.get("epochs", 600),
         verbose=verbose,
         weight_decay=params.get("weight_decay", 0.001),
         dataset_name=tab_dataset.name,
@@ -87,6 +94,9 @@ def run_gnc(
     evaluate_metrics: bool = True,
     probs: bool = False,
     to_numpy: bool = True,
+    f2m: bool = False,
+    find_beta: bool = False,
+    n_gcn_layers: int = 1,
 ):
     graphs_dataset, inter_samples_edges, masks = GraphsDataset.from_tab(
         tab_data=tab_dataset,
@@ -96,6 +106,7 @@ def run_gnc(
         },
         inter_sample_edges=inter_sample_edges,
         calc_intra_edges=True,
+        f2m=f2m
     )
 
     train_mask, val_mask, test_mask = masks
@@ -112,11 +123,9 @@ def run_gnc(
     if params["gc_pretrain"]:
         gc_params = params["gc_params"]
 
-        train_loader = graphs_dataset.train.to_loader(
-            batch_size=gc_params["batch_size"]
-        )
-        val_loader = graphs_dataset.val.to_loader(batch_size=gc_params["batch_size"])
-        test_loader = graphs_dataset.test.to_loader(batch_size=gc_params["batch_size"])
+        train_loader = graphs_dataset.get_train_data(as_loader=True, batch_size=gc_params["batch_size"])
+        val_loader = graphs_dataset.get_val_data(as_loader=True, batch_size=gc_params["batch_size"])
+        test_loader = graphs_dataset.get_test_data(as_loader=True, batch_size=gc_params["batch_size"])
 
         RECEIVED_PARAMS = {
             "preweight": gc_params["preweight"],
@@ -131,6 +140,7 @@ def run_gnc(
             RECEIVED_PARAMS=RECEIVED_PARAMS,
             init_weights=gc_params.get("init", "xavier_normal"),
             embedding_layer=params["embedding_layer"],  # embedding_layer,
+            n_gcn_layers=n_gcn_layers,
         )
 
         if "gc_early_stopping" in gc_params.keys():
@@ -144,7 +154,7 @@ def run_gnc(
             test_loader=test_loader,
             lr=gc_params.get("lr", 0.01),
             early_stopping=gc_early_stopping,
-            n_epochs=600,
+            n_epochs=gc_params.get("epochs", 600),
             verbose=verbose,
             weight_decay=gc_params.get("weight_decay", 0.001),
             dataset_name=tab_dataset.name,
@@ -164,6 +174,7 @@ def run_gnc(
                 "dropout": params["gc_dropout"],
             },
             "embedding_layer": params["embedding_layer"],  # embedding_layer,
+            "n_gcn_layers": n_gcn_layers,
         },
         nc_kwargs={
             "h_layers": nc_h_layers,
@@ -181,7 +192,7 @@ def run_gnc(
         train_mask,
         val_mask,
         test_mask,
-        n_epochs=100,
+        n_epochs=params.get("epochs", 100),
         gc_lr=params["gc_lr"],
         nc_lr=params["nc_lr"],
         gc_weight_decay=params["gc_weight_decay"],
@@ -191,6 +202,7 @@ def run_gnc(
         early_stopping=early_stopping,
         verbose=verbose,
         clf_from=params["clf_from"],  # clf_from
+        find_beta=find_beta,
     )
 
     y_test = model.predict(
@@ -204,7 +216,7 @@ def run_gnc(
 
     if evaluate_metrics and verbose == 1:
         print(
-            f"Test accuracy: {(test_loader.dataset.gdp.Y.flatten() == y_test).float().mean():.4f}"
+            f"Test accuracy: {(all_graphs_loader.dataset.gdp.Y.flatten()[test_mask] == y_test).float().mean():.4f}"
         )
 
     if probs:
@@ -228,6 +240,8 @@ def run_gc_nc(
     evaluate_metrics: bool = True,
     probs: bool = False,
     to_numpy: bool = True,
+    f2m: bool = False,
+    n_gcn_layers: int = 1,
 ):
     graphs_dataset, inter_sample_edges, masks = GraphsDataset.from_tab(
         tab_data=tab_dataset,
@@ -237,14 +251,15 @@ def run_gc_nc(
         },
         inter_sample_edges=inter_sample_edges,
         calc_intra_edges=True,
+        f2m=f2m
     )
 
     train_graphs_loader = graphs_dataset.train.to_loader(
         batch_size=params["batch_size"]
     )
 
-    val_graphs_loader = graphs_dataset.val.to_loader(batch_size=params["batch_size"])
-    test_graphs_loader = graphs_dataset.test.to_loader(batch_size=params["batch_size"])
+    val_graphs_loader = graphs_dataset.get_val_data(as_loader=True, batch_size=params["batch_size"])
+    test_graphs_loader = graphs_dataset.get_test_data(as_loader=True, batch_size=params["batch_size"])
 
     GC_RECEIVED_PARAMS = {
         "preweight": params["gc_preweight"],
@@ -259,6 +274,7 @@ def run_gc_nc(
         RECEIVED_PARAMS=GC_RECEIVED_PARAMS,
         init_weights=params.get("init", "xavier_normal"),
         embedding_layer=params["embedding_layer"],  # embedding_layer
+        n_gcn_layers=n_gcn_layers,
     )
 
     gc_early_stopping = params.get("gc_early_stopping", 30)
@@ -269,13 +285,16 @@ def run_gc_nc(
         test_loader=test_graphs_loader,
         lr=params["gc_lr"],
         early_stopping=gc_early_stopping,
-        n_epochs=600,
+        n_epochs=params.get("gc_epochs", 600),
         verbose=verbose,
         weight_decay=params.get("gc_weight_decay", 0),
         dataset_name=tab_dataset.name,
     )
 
     def extract_embeddings(graphs_loader):
+        if graphs_loader is None:
+            return None
+
         first_batch = True
 
         for graphs_data in graphs_loader:
@@ -326,28 +345,48 @@ def run_gc_nc(
 
     inter_samples_edges_train = torch.LongTensor(inter_samples_edges_train)
 
-    all_embeddings = torch.cat([train_embeddings, val_embeddings, test_embeddings])
+    if val_embeddings is None:
+        all_embeddings = torch.cat([train_embeddings, test_embeddings])
 
-    train_graph = [
-        (
-            (train_embeddings, inter_samples_edges_train.T, masks[0]),
-            tab_dataset.train.Y,
-        )
-    ]
+        train_graph = [
+            (
+                (train_embeddings, inter_samples_edges_train.T, masks[0]),
+                tab_dataset.train.Y,
+            )
+        ]
 
-    val_graph = [
-        (
-            (all_embeddings, inter_sample_edges.T, masks[1]),
-            tab_dataset.val.Y,
-        )
-    ]
+        test_graph = [
+            (
+                (all_embeddings, inter_sample_edges.T, masks[2]),
+                tab_dataset.test.Y,
+            )
+        ]
 
-    test_graph = [
-        (
-            (all_embeddings, inter_sample_edges.T, masks[2]),
-            tab_dataset.test.Y,
-        )
-    ]
+        val_graph = None
+
+    else:
+        all_embeddings = torch.cat([train_embeddings, val_embeddings, test_embeddings])
+
+        train_graph = [
+            (
+                (train_embeddings, inter_samples_edges_train.T, masks[0]),
+                tab_dataset.train.Y,
+            )
+        ]
+
+        val_graph = [
+            (
+                (all_embeddings, inter_sample_edges.T, masks[1]),
+                tab_dataset.val.Y,
+            )
+        ]
+
+        test_graph = [
+            (
+                (all_embeddings, inter_sample_edges.T, masks[2]),
+                tab_dataset.test.Y,
+            )
+        ]
 
     nc_early_stopping = params.get("nc_early_stopping", 30)
 
@@ -356,7 +395,7 @@ def run_gc_nc(
         val_loader=val_graph,
         test_loader=test_graph,
         lr=params["nc_lr"],
-        n_epochs=600,
+        n_epochs=params.get("nc_epochs", 600),
         early_stopping=nc_early_stopping,
         weight_decay=params.get("nc_weight_decay", 0),
         verbose=verbose,

build/lib/spagog/models/abstractNN.py
@@ -142,6 +142,10 @@ class AbstractNN(nn.Module, AbstractModel):
     ):
         assert metric in ["auc", "accuracy"]
 
+        if early_stopping > 0 and val_loader is None:
+            print("Early stopping is off because val_loader is None")
+            early_stopping = -1
+
         self.train()
         train_losses, val_losses = [], []
         train_aucs, val_aucs = [], []
@@ -193,33 +197,6 @@ class AbstractNN(nn.Module, AbstractModel):
             t_per_train_epoch.append(time.time() - t_train)
             total_train_loss = total_train_loss / (len(train_loader.dataset) if isinstance(train_loader, DataLoader) else train_loader[0][1].shape[0])
 
-            t_val = time.time()
-
-            with torch.no_grad():
-                total_val_loss = 0
-
-                for b_num_val, data in enumerate(val_loader):
-                    input_data, labels = self._transform_input(data)
-                    output = self(*input_data)
-
-                    loss = self._eval_loss(
-                        output,
-                        labels,
-                        criterion,
-                        n_classes=self.get_num_classes(),
-                    )
-
-                    total_val_loss += loss.item() * (
-                        input_data[0].size(0)
-                        / (1 if criterion.reduction == "sum" else input_data[0].size(0))
-                    )
-
-            t_per_val_epoch.append(time.time() - t_val)
-
-            total_val_loss = total_val_loss / (len(val_loader.dataset) if isinstance(val_loader, DataLoader) else val_loader[0][1].shape[0])
-
-            t_auc = time.time()
-
             train_auc = self.evaluate(
                 loader=train_loader,
                 metric=metric,
@@ -227,27 +204,54 @@ class AbstractNN(nn.Module, AbstractModel):
                 to_numpy=False
             )
 
-            val_auc = self.evaluate(
-                loader=val_loader, metric=metric, labels_from_loader=labels_from_loader, to_numpy=False
-            )
+            if val_loader is not None:
+                t_val = time.time()
 
-            t_per_auc_epoch.append(time.time() - t_auc)
+                with torch.no_grad():
+                    total_val_loss = 0
 
-            train_losses.append(total_train_loss)
-            val_losses.append(total_val_loss)
-            train_aucs.append(train_auc)
-            val_aucs.append(val_auc)
+                    for b_num_val, data in enumerate(val_loader):
+                        input_data, labels = self._transform_input(data)
+                        output = self(*input_data)
+
+                        loss = self._eval_loss(
+                            output,
+                            labels,
+                            criterion,
+                            n_classes=self.get_num_classes(),
+                        )
+
+                        total_val_loss += loss.item() * (
+                            input_data[0].size(0)
+                            / (1 if criterion.reduction == "sum" else input_data[0].size(0))
+                        )
+
+                t_per_val_epoch.append(time.time() - t_val)
+
+                total_val_loss = total_val_loss / (len(val_loader.dataset) if isinstance(val_loader, DataLoader) else val_loader[0][1].shape[0])
+
+                val_auc = self.evaluate(
+                    loader=val_loader, metric=metric, labels_from_loader=labels_from_loader, to_numpy=False
+                )
+
+                val_losses.append(total_val_loss)
+                val_aucs.append(val_auc)
+
 
             if verbose:
                 print(
                     f"Epoch {epoch}/{n_epochs}:\n"
                     f"\tTrain loss: {total_train_loss:.4f}\n"
-                    f"\tVal loss: {total_val_loss:.4f}\n"
-                    f"\tTrain AUC: {train_auc:.4f}\n"
-                    f"\tVal AUC: {val_auc:.4f}"
+                    f"\tTrain AUC: {train_auc:.4f}" + ('\n' if val_loader is None else "")
                 )
 
-            if early_stopping > 0:
+                if val_loader is not None:
+                    print(
+                        f"\tVal loss: {total_val_loss:.4f}\n"
+                        f"\tVal AUC: {val_auc:.4f}\n"
+                    )
+
+            if early_stopping > 0 and val_loader is not None:
                 if val_auc > max_val_auc:
                     max_val_auc = val_auc
                     c = 0
@@ -273,7 +277,7 @@ class AbstractNN(nn.Module, AbstractModel):
 
         results_cache["learning_epochs"] = epoch
         results_cache["Train AUC"] = train_auc
-        results_cache["Val AUC"] = val_auc
+        results_cache["Val AUC"] = None if val_loader is None else val_auc
 
         if test_loader is not None and labels_from_loader(test_loader) is not None:
             test_auc = self.evaluate(
@@ -345,11 +349,12 @@ class AbstractNN(nn.Module, AbstractModel):
     ):
         given_test_y = test_loader is not None and labels_from_loader(test_loader) is not None
 
-        train_labels = labels_from_loader(train_loader).view(-1, 1)
-        val_labels = labels_from_loader(val_loader).view(-1, 1)
-
+        train_labels = labels_from_loader(train_loader).view(-1, 1).long()
         final_output_train = self.predict(loader=train_loader, probs=True, **kwargs)
-        final_output_val = self.predict(loader=val_loader, probs=True, **kwargs)
+
+        if val_loader is not None:
+            val_labels = labels_from_loader(val_loader).view(-1, 1)
+            final_output_val = self.predict(loader=val_loader, probs=True, **kwargs)
 
         if given_test_y:
             test_labels = labels_from_loader(test_loader).view(-1, 1)
@@ -369,19 +374,23 @@ class AbstractNN(nn.Module, AbstractModel):
                 threshold=None,
             )
 
-            pos_output_val = nn.Sigmoid()(final_output_val[:, 1])
+            if val_loader is not None:
+                pos_output_val = nn.Sigmoid()(final_output_val[:, 1])
 
-            best_val_acc, *_ = find_best_metrics_bin(
-                pos_output_val,
-                val_labels,
-                threshold=best_acc_threshold,
-            )
+                best_val_acc, *_ = find_best_metrics_bin(
+                    pos_output_val,
+                    val_labels,
+                    threshold=best_acc_threshold,
+                )
 
-            _, best_val_f1, *_ = find_best_metrics_bin(
-                pos_output_val,
-                val_labels,
-                threshold=best_f1_threshold,
-            )
+                _, best_val_f1, *_ = find_best_metrics_bin(
+                    pos_output_val,
+                    val_labels,
+                    threshold=best_f1_threshold,
+                )
+
+            else:
+                best_val_acc = best_val_f1 = None
 
             if given_test_y:
                 pos_output_test = nn.Sigmoid()(final_output_test[:, 1])
@@ -417,7 +426,12 @@ class AbstractNN(nn.Module, AbstractModel):
             return cache
 
         train_acc, train_f1 = find_best_metrics_multi(final_output_train, train_labels)
-        val_acc, val_f1 = find_best_metrics_multi(final_output_val, val_labels)
+
+        if val_loader is not None:
+            val_acc, val_f1 = find_best_metrics_multi(final_output_val, val_labels)
+
+        else:
+            val_acc, val_f1 = None, None
 
         cache = {
             "Train Acc": train_acc,
@@ -445,9 +459,16 @@ class AbstractNN(nn.Module, AbstractModel):
         labels_from_loader: Callable = lambda loader: loader.dataset.gdp.Y,
         **kwargs
     ) -> float:
+
+        if loader is None:
+            return None
+
         self.eval()
         preds = self.predict(loader, probs=True, **kwargs)
-        labels = labels_from_loader(loader).view(-1, 1)
+        labels = labels_from_loader(loader).view(-1, 1).long()
+
+        if labels is None:
+            return None
 
         labels_ = torch.zeros_like(preds)
         labels_[range(labels.shape[0]), labels[:, 0]] = 1

build/lib/spagog/utils/data/tab2graph.py
@@ -25,7 +25,8 @@ def tab2graphs(
         val_mask: list = None,
         test_mask: list = None,
         inter_sample_edges: torch.Tensor = None,
-        calc_intra_edges: bool = True
+        calc_intra_edges: bool = True,
+        f2m: bool = False,
 ):
     assert not include_edge_weights or edge_weights_method in ["corr", None]
     assert fill_data_method in ["gfp", "zeros"]
@@ -72,18 +73,34 @@ def tab2graphs(
     X_list = list(imputed_data_)
 
     if calc_intra_edges:
-        edge_list = list(
-            map(
-                lambda s: torch.from_numpy(
-                    lst_to_mat(
-                        np.array(list(combinations(np.where(s.cpu() == s.cpu())[0], r=2))),
-                        len(s),
-                        edge_weights,
-                    )
-                ),
-                list(tab_data_),
+        if f2m:
+            edge_list = (
+                torch.from_numpy(edge_weights)
+                .unsqueeze(0)
+                .repeat([tab_data_.shape[0], 1, 1])
             )
-        )
+
+            masks = torch.repeat_interleave(
+                ~torch.isnan(tab_data_).unsqueeze(-1), tab_data_.shape[1], dim=2
+            ).long()
+            edge_list = edge_list * masks
+            edge_list = list(edge_list)
+
+        else:
+            edge_list = list(
+                map(
+                    lambda s: torch.from_numpy(
+                        lst_to_mat(
+                            np.array(
+                                list(combinations(np.where(s.cpu() == s.cpu())[0], r=2))
+                            ),
+                            len(s),
+                            edge_weights,
+                        )
+                    ),
+                    list(tab_data_),
+                )
+            )
 
     else:
         edge_list = None

spagog/gog_model.py
@@ -7,7 +7,13 @@ import torch
 import pandas as pd
 import numpy as np
 
-from .experiments import run_gc, run_gnc, run_gc_nc, get_default_params_file, get_tab_data
+from .experiments import (
+    run_gc,
+    run_gnc,
+    run_gc_nc,
+    get_default_params_file,
+    get_tab_data,
+)
 
 warnings.filterwarnings("ignore")
 
@@ -16,24 +22,33 @@ os.chdir(PROJECT_DIR)
 
 
 def gog_model(
-        model: str,
-        train_X: pd.DataFrame,
-        train_Y: pd.DataFrame,
-        test_X: pd.DataFrame,
-        test_Y: pd.DataFrame = None,
-        val_X: pd.DataFrame = None,
-        val_Y: pd.DataFrame = None,
-        evaluate_metrics: bool = True,
-        dataset_name: str = "",
-        feature_selection: int = 100,
-        edges: pd.DataFrame = None,
-        probs: bool = False,
-        to_numpy: bool = False,
-        verbosity: int = 0,
-        **spec_params
+    model: str,
+    train_X: pd.DataFrame,
+    train_Y: pd.DataFrame,
+    test_X: pd.DataFrame,
+    test_Y: pd.DataFrame = None,
+    val_X: pd.DataFrame = None,
+    val_Y: pd.DataFrame = None,
+    evaluate_metrics: bool = True,
+    dataset_name: str = "",
+    feature_selection: int = 100,
+    edges: pd.DataFrame = None,
+    probs: bool = False,
+    to_numpy: bool = False,
+    verbosity: int = 0,
+    f2m: bool = False,
+    find_beta: bool = False,
+    n_gcn_layers: int = 1,
+    **spec_params,
 ):
-    assert not evaluate_metrics or test_Y is not None, "Please provide test_Y to evaluate metrics"
-    assert model in ["gc", "gnc", "gc+nc"], "Please provide a valid model {gc, gnc, gc+nc}}"
+    assert (
+        not evaluate_metrics or test_Y is not None
+    ), "Please provide test_Y to evaluate metrics"
+    assert model in [
+        "gc",
+        "gnc",
+        "gc+nc",
+    ], "Please provide a valid model {gc, gnc, gc+nc}}"
     assert verbosity in [0, 1, 2], "Please provide a valid verbosity level {0, 1, 2}"
 
     if verbosity > 0:
@@ -60,10 +75,17 @@ def gog_model(
         elif "gc_params" in params.keys() and k in params["gc_params"].keys():
             params["gc_params"][k] = v
 
-    assert "embedding_layer" not in params.keys() or params["embedding_layer"] in ['one_before_last', 'first', 'mid']
-    assert "gc_params" not in params.keys() or "embedding_layer" not in params["gc_params"].keys() or \
-           params["gc_params"]["embedding_layer"] in ['one_before_last', 'first', 'mid']
-    assert "clf_from" not in params.keys() or params["clf_from"] in ['gc', 'nc']
+    assert "embedding_layer" not in params.keys() or params["embedding_layer"] in [
+        "one_before_last",
+        "first",
+        "mid",
+    ]
+    assert (
+        "gc_params" not in params.keys()
+        or "embedding_layer" not in params["gc_params"].keys()
+        or params["gc_params"]["embedding_layer"] in ["one_before_last", "first", "mid"]
+    )
+    assert "clf_from" not in params.keys() or params["clf_from"] in ["gc", "nc"]
 
     tab_dataset = get_tab_data(
         train_X=train_X,
@@ -86,62 +108,65 @@ def gog_model(
     else:
         run_func = run_gc_nc
 
-    y_test, res_cache = run_func(
-        tab_dataset,
-        params,
-        inter_sample_edges=inter_sample_edges,
-        verbose=verbosity == 2,
-        evaluate_metrics=evaluate_metrics,
-        probs=probs,
-        to_numpy=to_numpy,
-    )
+    kwargs = {
+        "tab_dataset": tab_dataset,
+        "params": params,
+        "inter_sample_edges": inter_sample_edges,
+        "verbose": verbosity == 2,
+        "evaluate_metrics": evaluate_metrics,
+        "probs": probs,
+        "to_numpy": to_numpy,
+        "f2m": f2m,
+        "n_gcn_layers": n_gcn_layers,
+    }
+
+    if model == "gnc":
+        kwargs["find_beta"] = find_beta
+
+    y_test, res_cache = run_func(**kwargs)
 
     if verbosity == 2:
         print()
 
     if verbosity > 0:
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        print(f"Results on {dataset_name.upper() if dataset_name != '' else 'dataset'} with {model.upper()}:")
+        print(
+            f"Results on {dataset_name.upper() if dataset_name != '' else 'dataset'} with {model.upper()}:"
+        )
         print(f"\tN epochs:\t{res_cache['learning_epochs']}")
         print("\tAccuracy:")
         print(
             f"\t\tThreshold:\t{res_cache['Acc Threshold']}\n"
             f"\t\tTrain:\t{res_cache['Train Acc']}\n"
-            f"\t\tVal:\t{res_cache['Val Acc']}"
+            f"\t\tVal:\t{res_cache.get('Val Acc')}"
         )
 
         if evaluate_metrics:
-            print(
-                f"\t\tTest:\t{res_cache['Test Acc']}"
-            )
+            print(f"\t\tTest:\t{res_cache['Test Acc']}")
 
         print("\tF1:")
         print(
             f"\t\tThreshold:\t{res_cache['F1 Threshold']}\n"
             f"\t\tTrain:\t{res_cache['Train F1']}\n"
-            f"\t\tVal:\t{res_cache['Val F1']}"
+            f"\t\tVal:\t{res_cache.get('Val F1')}"
         )
 
         if evaluate_metrics:
-            print(
-                f"\t\tTest:\t{res_cache['Test F1']}"
-            )
+            print(f"\t\tTest:\t{res_cache['Test F1']}")
 
         print("\tAUC:")
         print(
             f"\t\tTrain:\t{res_cache['Train AUC']}\n"
-            f"\t\tVal:\t{res_cache['Val AUC']}"
+            f"\t\tVal:\t{res_cache.get('Val AUC')}"
         )
 
         if evaluate_metrics:
-            print(
-                f"\t\tTest:\t{res_cache['Test AUC']}"
-            )
+            print(f"\t\tTest:\t{res_cache['Test AUC']}")
 
         print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
     if verbosity > 0:
-        _et  = time.time()
+        _et = time.time()
         diff = _et - _st
 
         _sec = np.round(diff % 60, 2)
@@ -171,6 +196,14 @@ if __name__ == "__main__":
     val_Y = val_all[target_col]
     val_X = val_all.drop(target_col, axis=1)
 
-    results = gog_model(train_X=train_X, train_Y=train_Y, test_X=test_X, test_Y=test_Y, val_X=val_X,
-                        val_Y=val_Y, model="gc", evaluate_metrics=test_Y is not None,
-                        verbosity=1)
+    results = gog_model(
+        train_X=train_X,
+        train_Y=train_Y,
+        test_X=test_X,
+        test_Y=test_Y,
+        val_X=val_X,
+        val_Y=val_Y,
+        model="gc",
+        evaluate_metrics=test_Y is not None,
+        verbosity=1,
+    )

spagog/models/graphClassification.py
@@ -22,7 +22,8 @@ class ValuesAndGraphStructure(AbstractNN):
             num_classes: int = 1,
             input_example: Union[DataLoader, GraphsDataset] = None,
             init_weights="xavier_normal",
-            embedding_layer: str = 'one_before_last'
+            embedding_layer: str = 'one_before_last',
+            n_gcn_layers: int = 1,
     ):
         super(ValuesAndGraphStructure, self).__init__()
 
@@ -43,9 +44,13 @@ class ValuesAndGraphStructure(AbstractNN):
         self.num_classes = max([self.num_classes, 2])
         self.RECEIVED_PARAMS = RECEIVED_PARAMS
 
-        self.pre_weighting = nn.Linear(1, int(self.RECEIVED_PARAMS["preweight"])).to(
+        self.first_pre_weighting = nn.Linear(1, int(self.RECEIVED_PARAMS["preweight"])).to(
             self.device
         )
+
+        self.next_pre_weighting = [nn.Linear(
+            int(self.RECEIVED_PARAMS["preweight"]), int(self.RECEIVED_PARAMS["preweight"])
+        ).to(self.device) for _ in range(n_gcn_layers - 1)]
 
         self.fc1 = nn.Linear(
             int(self.RECEIVED_PARAMS["preweight"]) * self.nodes_number,
@@ -77,9 +82,13 @@ class ValuesAndGraphStructure(AbstractNN):
         if init_weights is not None:
             self.init_weights(init_weights)
 
-        self.gcn_layer = nn.Sequential(
-            self.pre_weighting, self.activation_func_dict[self.activation_func]
+        self.first_gcn_layer = nn.Sequential(
+            self.first_pre_weighting, self.activation_func_dict[self.activation_func]
         ).to(self.device)
+
+        self.next_gcn_layers = nn.ModuleList([nn.Sequential(
+            next_pre_weighting, self.activation_func_dict[self.activation_func]
+        ).to(self.device) for next_pre_weighting in self.next_pre_weighting])
 
         self.embedding_layer = embedding_layer
 
@@ -110,7 +119,11 @@ class ValuesAndGraphStructure(AbstractNN):
             -1
         )
 
-        x = self.gcn_layer(x)
+        x = self.first_gcn_layer(x)
+
+        for next_gcn_layer in self.next_gcn_layers:
+            x = next_gcn_layer(x)
+
         x = torch.flatten(x, start_dim=1)
 
         if self.embedding_layer != 'first':

spagog/models/graphNodeClassification.py
@@ -1,5 +1,6 @@
 import sys
 import numpy as np
+from tqdm import tqdm
 import matplotlib.pyplot as plt
 
 import torch
@@ -169,6 +170,7 @@ class GraphNodeClassification(nn.Module, AbstractModel):
         gc_weight_decay: float = 0,
         nc_lr: float = 0.001,
         nc_weight_decay: float = 0,
+        find_beta: bool = True,
     ):
 
         given_test_y = graphs_dataset.test.Y is not None
@@ -394,56 +396,104 @@ class GraphNodeClassification(nn.Module, AbstractModel):
                 test_mask,
             )
 
-        if clf_from == "nc":
-            final_output_train = nn.Softmax(dim=1)(final_output_train)
+        if find_beta:
+            best_beta, best_loss = -1, np.inf
 
-            if graphs_val_loader is not None:
-                final_output_val = nn.Softmax(dim=1)(final_output_val)
+            for beta in tqdm.tqdm(
+                np.array(range(0, 101)) / 100, desc="Searching for best beta"
+            ):
+                output = beta * final_output_train + (1 - beta) * gc_output_train
+                train_loss = nn.CrossEntropyLoss()(
+                    output, train_labels.long().view(-1)
+                ).item()
 
-            if given_test_y:
-                final_output_test = nn.Softmax(dim=1)(final_output_test)
+                if train_loss < best_loss:
+                    best_loss = train_loss
+                    best_beta = beta
+
+            if verbose:
+                print(f"Best beta is {best_beta} with loss of {best_loss} on training")
 
         else:
-            gc_output_train = nn.Softmax(dim=1)(gc_output_train)
+            best_beta = int(clf_from == "nc")
 
-            if graphs_val_loader is not None:
-                gc_output_val = nn.Softmax(dim=1)(gc_output_val)
-
-            if given_test_y:
-                gc_output_test = nn.Softmax(dim=1)(gc_output_test)
-
-        train_auc = roc_auc_score(
-            one_hot(train_labels.view(-1).long(), n_classes).cpu().detach().numpy(),
-            (final_output_train if clf_from == "nc" else gc_output_train)
-            .cpu()
-            .detach()
-            .numpy(),
+        mixed_output_train = (
+            best_beta * final_output_train + (1 - best_beta) * gc_output_train
         )
 
         if graphs_val_loader is not None:
-            val_auc = roc_auc_score(
-                one_hot(val_labels.view(-1).long(), n_classes).cpu().detach().numpy(),
-                (final_output_val if clf_from == "nc" else gc_output_val)
-                .cpu()
-                .detach()
-                .numpy(),
+            mixed_output_val = (
+                best_beta * final_output_val + (1 - best_beta) * gc_output_val
             )
 
         if given_test_y:
-            test_auc = roc_auc_score(
-                one_hot(test_labels.view(-1).long(), n_classes).cpu().detach().numpy(),
-                (final_output_test if clf_from == "nc" else gc_output_test)
-                .cpu()
-                .detach()
-                .numpy(),
+            mixed_output_test = (
+                best_beta * final_output_test + (1 - best_beta) * gc_output_test
             )
 
-        if self.bin:
-            if clf_from == "nc":
-                pos_output_train = final_output_train[:, 1]
+        try:
+            train_auc = roc_auc_score(
+                one_hot(train_labels.view(-1).long(), n_classes).cpu().detach().numpy(),
+                mixed_output_train.cpu().detach().numpy(),
+            )
 
-            else:
-                pos_output_train = gc_output_train[:, 1]
+        except:
+            train_auc = None
+
+        try:
+            if graphs_val_loader is not None:
+                val_auc = roc_auc_score(
+                    one_hot(val_labels.view(-1).long(), n_classes).cpu().detach().numpy(),
+                    (final_output_val if clf_from == "nc" else gc_output_val)
+                    .cpu()
+                    .detach()
+                    .numpy(),
+                )
+
+        except:
+            val_auc = None
+
+        if given_test_y:
+            try:
+                test_auc = roc_auc_score(
+                    one_hot(test_labels.view(-1).long(), n_classes)
+                    .cpu()
+                    .detach()
+                    .numpy(),
+                    mixed_output_test.cpu().detach().numpy(),
+                )
+
+            except:
+                test_auc = None
+
+        # train_auc = roc_auc_score(
+        #     one_hot(train_labels.view(-1).long(), n_classes).cpu().detach().numpy(),
+        #     (final_output_train if clf_from == "nc" else gc_output_train)
+        #     .cpu()
+        #     .detach()
+        #     .numpy(),
+        # )
+
+        # if graphs_val_loader is not None:
+        #     val_auc = roc_auc_score(
+        #         one_hot(val_labels.view(-1).long(), n_classes).cpu().detach().numpy(),
+        #         (final_output_val if clf_from == "nc" else gc_output_val)
+        #         .cpu()
+        #         .detach()
+        #         .numpy(),
+        #     )
+
+        # if given_test_y:
+        #     test_auc = roc_auc_score(
+        #         one_hot(test_labels.view(-1).long(), n_classes).cpu().detach().numpy(),
+        #         (final_output_test if clf_from == "nc" else gc_output_test)
+        #         .cpu()
+        #         .detach()
+        #         .numpy(),
+        #     )
+
+        if self.bin:
+            pos_output_train = mixed_output_train[:, 1]
 
             (
                 best_train_acc,
@@ -457,11 +507,7 @@ class GraphNodeClassification(nn.Module, AbstractModel):
             )
 
             if graphs_val_loader is not None:
-                if clf_from == "nc":
-                    pos_output_val = final_output_val[:, 1]
-
-                else:
-                    pos_output_val = gc_output_val[:, 1]
+                pos_output_val = mixed_output_val[:, 1]
 
                 best_val_acc, *_ = find_best_metrics_bin(
                     pos_output_val,
@@ -479,10 +525,7 @@ class GraphNodeClassification(nn.Module, AbstractModel):
                 best_val_acc, best_val_f1 = None, None
 
             if given_test_y:
-                if clf_from == "nc":
-                    pos_output_test = final_output_test[:, 1]
-                else:
-                    pos_output_test = gc_output_test[:, 1]
+                pos_output_test = mixed_output_test[:, 1]
 
                 best_test_acc, *_ = find_best_metrics_bin(
                     pos_output_test,
@@ -497,40 +540,19 @@ class GraphNodeClassification(nn.Module, AbstractModel):
                 )
 
         else:
-            if clf_from == "nc":
-                best_train_acc, best_train_f1 = find_best_metrics_multi(
-                    final_output_train, train_labels
+            best_train_acc, best_train_f1 = find_best_metrics_multi(
+                mixed_output_train, train_labels
+            )
+
+            if graphs_val_loader is not None:
+                best_val_acc, best_val_f1 = find_best_metrics_multi(
+                    mixed_output_val, val_labels
                 )
 
-                if graphs_val_loader is not None:
-                    best_val_acc, best_val_f1 = find_best_metrics_multi(
-                        final_output_val, val_labels
-                    )
-
-                else:
-                    best_val_acc, best_val_f1 = None, None
-
-                if given_test_y:
-                    best_test_acc, best_test_f1 = find_best_metrics_multi(
-                        final_output_test, test_labels
-                    )
-
-            else:
-                best_train_acc, best_train_f1 = find_best_metrics_multi(
-                    gc_output_train, train_labels
+            if given_test_y:
+                best_test_acc, best_test_f1 = find_best_metrics_multi(
+                    mixed_output_test, test_labels
                 )
-
-                if graphs_val_loader is not None:
-                    best_val_acc, best_val_f1 = find_best_metrics_multi(
-                        gc_output_val, val_labels
-                    )
-                else:
-                    best_val_acc, best_val_f1 = None, None
-
-                if given_test_y:
-                    best_test_acc, best_test_f1 = find_best_metrics_multi(
-                        gc_output_test, test_labels
-                    )
 
             best_acc_threshold, best_f1_threshold = -1, -1
 
@@ -552,15 +574,21 @@ class GraphNodeClassification(nn.Module, AbstractModel):
 
         cache = {
             "Train Acc": best_train_acc,
-            "Val Acc": best_val_acc,
+            # "Val Acc": best_val_acc,
             "Train F1": best_train_f1,
-            "Val F1": best_val_f1,
+            # "Val F1": best_val_f1,
             "Train AUC": train_auc,
-            "Val AUC": val_auc,
+            # "Val AUC": val_auc,
             "Acc Threshold": best_acc_threshold,
             "F1 Threshold": best_f1_threshold,
             "learning_epochs": epoch,
         }
+
+        if graphs_val_loader is not None:
+            cache["Val Acc"] = best_val_acc
+            cache["Val F1"] = best_val_f1
+            cache["Val AUC"] = val_auc
+
 
         if given_test_y:
             cache["Test Acc"] = best_test_acc